79936429571ffea249a92102b22b276e31633bfb
Andrei <denisenko.andrei.da@gmail.com>
2020-10-03 16:32:27 +0300

    feat: created min, max and avg functions for working with numbers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array =  []) {
+  const minNum = array.sort((a,b) => a-b)[0]
+  return minNum ? minNum : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  const minNum = array.sort((a,b) => b-a)[0]
+  return minNum ? minNum : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  const average = (array.reduce((acc, item) => acc + item, 0)) / array.length
+  return average ? average : 0
 }
